0ff03bb1067111b2982c1a8000ae0fb5ff3b9aa0
Arif <most.crush31@gmail.com>
2025-11-06 19:18:39 +0700

    final

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-// ====== DATA DESTINASI ======
+// DATA DESTINASI
 const destinations = [
-    { name: "Bali", image: "https://source.unsplash.com/400x300/?bali" },
-    { name: "Raja Ampat", image: "https://source.unsplash.com/400x300/?raja-ampat" },
-    { name: "Gunung Bromo", image: "https://source.unsplash.com/400x300/?bromo" },
-    { name: "Lombok", image: "https://source.unsplash.com/400x300/?lombok" },
-    { name: "Labuan Bajo", image: "https://source.unsplash.com/400x300/?labuan-bajo" },
+    { name: "Situ Patenggang", image: "../asset/situ.jpg" },
+    { name: "Ranca Upas", image: "../asset/RANCA.jpg" },
+    { name: "Kawah Putih", image: "../asset/Kawah.jpg" },
+    { name: "Ciwalini", image: "../asset/ciwalini.jpg" },
+    { name: "Ranca Bali", image: "../asset/rancabali.jpg" },
 ];
 
-// ====== MENAMPILKAN DESTINASI ======
+// MENAMPILKAN DESTINASI 
 const container = document.getElementById("destinations");
 
 destinations.forEach(dest => {
@@ -20,13 +20,13 @@ destinations.forEach(dest => {
     container.appendChild(card);
 });
 
-// ====== SCROLL KE DESTINASI ======
+//  SCROLL KE DESTINASI 
 const mulaiBtn = document.getElementById("mulaiBtn");
 mulaiBtn.addEventListener("click", () => {
     document.getElementById("destinasi").scrollIntoView({ behavior: "smooth" });
 });
 
-// ====== MENU TOGGLE UNTUK HP ======
+//  MENU TOGGLE UNTUK HP 
 const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.getElementById("navbar");
 
@@ -34,7 +34,7 @@ menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
 });
 
-// ====== FORM KONTAK ALERT ======
+// FORM KONTAK ALERT 
 const form = document.querySelector(".contact-form");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
